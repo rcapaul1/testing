1d6cedb0a2424057756e3712973c98f11966ecb4
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-03-01 14:23:06 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -7,8 +7,10 @@ hdparm -tT /dev/sdb2 --direct >>/root/speedtest_read.txt && echo "ext4 ohne Jour
 hdparm -tT /dev/sdb3 >>/root/speedtest_read.txt && echo "xfs mit Cache" >>/root/speedtest_read.txt
 hdparm -tT /dev/sdb3 --direct >>/root/speedtest_read.txt && echo "xfs ohne Cache" >>/root/speedtest_read.txt
 
-echo "Speedtest der Partitionen" | mailx -a /root/speedtest_read.txt -s "Speedtest" rcapaul@hosttech.ch
+echo "Speedtest Lesen der Partitionen" | mailx -a /root/speedtest_read.txt -s "Speedtest Read" rcapaul@hosttech.ch
 
 dd if=/dev/zero of=/dev/sdb1 bs=1M count=2048 conv=fdatasync,notrunc >/root/speedtest_write.txt && echo "ext4 mit Journal" >>/root/speedtest_write.txt
 dd if=/dev/zero of=/dev/sdb2 bs=1M count=2048 conv=fdatasync,notrunc >>/root/speedtest_write.txt && echo "ext4 ohne Journal" >> /root/speedtest_write.txt
 dd if=/dev/zero of=/dev/sdb3 bs=1M count=2048 conv=fdatasync,notrunc >>/root/speedtest_write.txt && echo "xfs" >> /root/speedtest_write.txt
+
+echo "Speedtest Schreiben der Partitionen" | mailx -a /root/speedtest_write.txt -s "Speedtest Write" rcapaul@hosttech.ch
