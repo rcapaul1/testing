6b195bfd89111a5721146a22e1e9fa14115f51f2
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-03-01 14:26:16 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -3,9 +3,9 @@
 hdparm -tT /dev/sdb1 >/root/speedtest_read.txt && echo "ext4 mit Journal mit Cache" >>/root/speedtest_read.txt
 hdparm -tT --direct /dev/sdb1 >>/root/speedtest_read.txt && echo "ext4 mit Journal ohne Cache" >>/root/speedtest_read.txt
 hdparm -tT /dev/sdb2 >>/root/speedtest_read.txt && echo "ext4 ohne Journal mit Cache" >>/root/speedtest_read.txt
-hdparm -tT /dev/sdb2 --direct >>/root/speedtest_read.txt && echo "ext4 ohne Journal ohne Cache" >>/root/speedtest_read.txt
+hdparm -tT --direct /dev/sdb2 >>/root/speedtest_read.txt && echo "ext4 ohne Journal ohne Cache" >>/root/speedtest_read.txt
 hdparm -tT /dev/sdb3 >>/root/speedtest_read.txt && echo "xfs mit Cache" >>/root/speedtest_read.txt
-hdparm -tT /dev/sdb3 --direct >>/root/speedtest_read.txt && echo "xfs ohne Cache" >>/root/speedtest_read.txt
+hdparm -tT --direct /dev/sdb3 >>/root/speedtest_read.txt && echo "xfs ohne Cache" >>/root/speedtest_read.txt
 
 echo "Speedtest Lesen der Partitionen" | mailx -a /root/speedtest_read.txt -s "Speedtest Read" rcapaul@hosttech.ch
 
