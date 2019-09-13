d0751275d530f8e4db1cd83dff21231dbd59f2dc
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-09-13 09:33:17 +0200

    Update apt-run.sh

--- apt-run.sh
+++ apt-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt upgrade -y && apt install git wget net-tools software-properties-common aptitude vim -y && aptitude update && aptitude safe-upgrade -y
+apt update && apt upgrade -y && apt install git wget net-tools software-properties-common aptitude dirmngr vim -y && aptitude update && aptitude safe-upgrade -y
