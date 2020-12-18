4cbb6e6b794b36e79d3215b9cd7c750ce511e0a4
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-12-18 13:52:44 +0100

    Update yum-run.sh

--- yum-run.sh
+++ yum-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp /root/testing/.bashrc .bashrc && cp /root/testing/.vimrc .vimrc && source /root/.bashrc && source /root/.vimrc
 yum install epel-release git wget net-tools yum-utils vim -y && yum update -y && yum remove firewalld -y && yum groupinstall "Development Tools" -y
