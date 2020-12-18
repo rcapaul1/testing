8e9dffccd47d000cc5f1837aa62c6e2300b27b8d
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-12-18 13:46:16 +0100

    Update apt-run.sh

--- apt-run.sh
+++ apt-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp /root/testing/.bashrc .bashrc && cp /root/testing/.vimrc .vimrc && source /root/.bashrc && source /root/.vimrc
 apt update && apt upgrade -y && apt install git wget net-tools software-properties-common aptitude dirmngr vim -y && aptitude update && aptitude safe-upgrade -y
