9bb1ddf0da42a85e50f2f134b8b01260392f8180
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2023-09-11 10:25:01 +0200

    Update yum-run.sh

--- yum-run.sh
+++ yum-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /root/.bashrc ; source /root/.vimrc
-yum install epel-release git wget net-tools yum-utils vim -y && yum update -y && yum remove firewalld -y && yum groupinstall "Development Tools" -y
+yum install epel-release git wget iperf3 net-tools yum-utils vim -y && yum update -y && yum remove firewalld -y && yum groupinstall "Development Tools" -y
