47036eb6a9adccd437c4c52b7700704db43e4a93
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2024-09-04 10:07:30 +0200

    Update apt-run.sh

--- apt-run.sh
+++ apt-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp /root/testing/.bashrc /root/.bashrc && cp /root/testing/.vimrc /root/.vimrc ; source /root/.bashrc ; source /root/.vimrc
-apt update && apt upgrade -y && apt install git wget calc net-tools software-properties-common aptitude dirmngr vim iperf3 -y && aptitude update && aptitude safe-upgrade -y && apt autoremove -y
+apt update && apt upgrade -y && apt install git wget calc net-tools software-properties-common aptitude dirmngr vim iperf3 nodejs npm -y && aptitude update && aptitude safe-upgrade -y && apt autoremove -y
