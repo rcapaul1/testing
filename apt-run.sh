e22df15c2f45678b5e49b1e18a62c46210bb3be0
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2026-07-17 11:09:20 +0200

    Update apt-run.sh

--- apt-run.sh
+++ apt-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /root/testing/.bashrc /root/.bashrc && cp /root/testing/.vimrc /root/.vimrc ; source /root/.bashrc ; source /root/.vimrc
+cp testing/.bashrc .bashrc && cp testing/.vimrc .vimrc ; source .bashrc ; source .vimrc
 apt update && apt upgrade -y && apt install git wget pandoc calc net-tools software-properties-common aptitude dirmngr whois dnsutils vim iperf3 nodejs npm -y && apt autoremove -y
